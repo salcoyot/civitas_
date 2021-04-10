15ee7942b662ff17bba3cdc1addd38185f08b796
Salvador <salvadormmedel@gmail.com>
2021-04-09 21:56:54 -0600

    change falses por false

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const options = {
  };
  app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://civitas-kechw.ondigitalocean.app"); 
-  res.header('Access-Control-Allow-Credentials', falsed);
+  res.header('Access-Control-Allow-Credentials', false);
   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method, Access-Control-Allow-Credentials');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');

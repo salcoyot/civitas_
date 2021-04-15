4a4427ddb5613731c3823e835d3f8d1a2b96acd5
Salvador <salvadormmedel@gmail.com>
2021-04-14 21:20:08 -0600

    socket.broadcast.to(connectedUserId).emit

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ io.on("connection", socket => {
   connectedUserMap.set(socket.id, { status:'online', name: 'none' }); 
   console.log("conect and map:");
   console.log(connectedUserMap);
-  socket.emit.to(connectedUserId)("connectedlist", connectedUserMap );
+  socket.broadcast.to(connectedUserId).emit("connectedlist", connectedUserMap );
  /*   socket.send("Hello!");*/
 
   // or with emit() and custom event names

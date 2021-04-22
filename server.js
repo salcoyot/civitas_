66495f43a593e6f67f3a09efefd16391b362508d
Salvador <salvadormmedel@gmail.com>
2021-04-21 20:16:52 -0600

    add comunicate

--- server.js
+++ server.js
@@ -60,6 +60,12 @@ io.on("connection", socket => {
     socket.to(data.sendto).emit("newuser", {"position":data.position, "user":data.user, "id":data.id, "imhere":true});
     //socket.broadcast.emit("newuser", {"position":data.position, "user":data.user, "id":data.id, "imhere":true});
   });
+  socket.on("comunicate", (data) => {
+    console.log("imhere");
+    console.log(data);
+    socket.to(data.sendto).emit("newcomm", {"position":data.position, "user":data.user, "id":data.id});
+    //socket.broadcast.emit("newuser", {"position":data.position, "user":data.user, "id":data.id, "imhere":true});
+  });
   // handle the event sent with socket.emit()
   /* socket.on("salutations", (elem1, elem2, elem3) => {
     console.log(elem1, elem2, elem3);
